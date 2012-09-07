3a9268ff680757f1cde5ccc414ac7718d39f1ed4
Mikhail Korobov <kmike84@gmail.com>
2012-09-08 03:31:51 +0600

    test refactoring + extra tests for exception raising

--- tests/test_dawg.py
+++ tests/test_dawg.py
@@ -19,107 +19,136 @@ def test_contains():
     assert b'foo' in d
     assert b'x' not in d
 
-def test_sorted_iterable():
+class TestDAWG(object):
 
-    sorted_data = ['bar', 'foo', 'foobar']
-    contents = "\n".join(sorted_data).encode('utf8')
-    with tempfile.NamedTemporaryFile() as f:
-        f.write(contents)
-        f.seek(0)
+    def test_sorted_iterable(self):
 
-        words = (line.strip() for line in f)
-        d = dawg.DAWG(words, input_is_sorted=True)
+        sorted_data = ['bar', 'foo', 'foobar']
+        contents = "\n".join(sorted_data).encode('utf8')
+        with tempfile.NamedTemporaryFile() as f:
+            f.write(contents)
+            f.seek(0)
 
-    assert 'bar' in d
-    assert 'foo' in d
+            words = (line.strip() for line in f)
+            d = dawg.DAWG(words, input_is_sorted=True)
+
+        assert 'bar' in d
+        assert 'foo' in d
+
+    def test_no_segfaults_on_invalid_file(self):
+        d = dawg.DAWG()
+        fd, path = tempfile.mkstemp()
+        with open(path, 'w') as f:
+            f.write('foo')
+
+        with pytest.raises(IOError) as e:
+            d.load(path)
+            assert 'Invalid' in e.args[0]
+
+        with open(path, 'rb') as f:
+            with pytest.raises(IOError) as e:
+                d.read(f)
+                assert 'Invalid' in e.args[0]
+
+class TestIntDAWG(object):
+
+    def dawg(self):
+        payload = {'foo': 1, 'bar': 5, 'foobar': 3}
+        d = dawg.IntDAWG(payload)
+        return payload, d
+
+    def test_getitem(self):
+        payload, d = self.dawg()
+        for key in payload:
+            assert d[key] == payload[key]
+
+        with pytest.raises(KeyError):
+            d['fo']
 
 
-def test_getitem():
-    d = dawg.IntDAWG({'foo': 1, 'bar': 5, 'foobar': 3})
-    assert d['foo'] == 1
-    assert d['bar'] == 5
-    assert d['foobar'] == 3
+    def test_dumps_loads(self):
+        payload, d = self.dawg()
+        data = d.tobytes()
 
-    with pytest.raises(KeyError):
-        d['fo']
+        d2 = dawg.IntDAWG()
+        d2.frombytes(data)
+        for key, value in payload.items():
+            assert key in d2
+            assert d2[key] == value
+
+    def test_dump_load(self):
+        payload, _ = self.dawg()
+
+        buf = BytesIO()
+        dawg.IntDAWG(payload).write(buf)
+        buf.seek(0)
+
+        d = dawg.IntDAWG()
+        d.read(buf)
+
+        for key, value in payload.items():
+            assert key in d
+            assert d[key] == value
+
+    def test_pickling(self):
+        payload, d = self.dawg()
+
+        data = pickle.dumps(d)
+        d2 = pickle.loads(data)
+
+        for key, value in payload.items():
+            assert key in d2
+            assert d[key] == value
+
+    def test_int_value_ranges(self):
+        for val in [0, 5, 2**16-1, 2**31-1]:
+            d = dawg.IntDAWG({'f': val})
+            assert d['f'] == val
+
+        with pytest.raises(ValueError):
+            dawg.IntDAWG({'f': -1})
+
+        with pytest.raises(OverflowError):
+            dawg.IntDAWG({'f': 2**32-1})
 
 
-def test_dumps_loads():
-    payload = {'foo': 1, 'bar': 5, 'foobar': 3}
-    d = dawg.IntDAWG(payload)
-    data = d.tobytes()
-
-    d2 = dawg.IntDAWG()
-    d2.frombytes(data)
-    for key, value in payload.items():
-        assert key in d2
-        assert d2[key] == value
-
-def test_dump_load():
-    payload = {'foo': 1, 'bar': 5, 'foobar': 3}
-
-    buf = BytesIO()
-    dawg.IntDAWG(payload).write(buf)
-    buf.seek(0)
-
-    d = dawg.IntDAWG()
-    d.read(buf)
-
-    for key, value in payload.items():
-        assert key in d
-        assert d[key] == value
-
-def test_pickling():
-    payload = {'foo': 1, 'bar': 5, 'foobar': 3}
-    d = dawg.IntDAWG(payload)
-
-    data = pickle.dumps(d)
-    d2 = pickle.loads(data)
-
-    for key, value in payload.items():
-        assert key in d2
-        assert d[key] == value
-
-def test_completion():
+class TestCompletionDAWG(object):
     keys = ['f', 'bar', 'foo', 'foobar']
-    d = dawg.CompletionDAWG(keys)
-    for key in keys:
-        assert key in d
 
-    assert d.keys('foo') == ['foo', 'foobar']
-    assert d.keys('b') == ['bar']
-    assert d.keys('z') == []
+    def dawg(self):
+        return dawg.CompletionDAWG(self.keys)
 
-def test_int_value_ranges():
-    for val in [0, 5, 2**16-1, 2**31-1]:
-        d = dawg.IntDAWG({'f': val})
-        assert d['f'] == val
+    def test_completion(self):
+        keys, d = self.keys, self.dawg()
+        for key in keys:
+            assert key in d
 
-    with pytest.raises(ValueError):
-        dawg.IntDAWG({'f': -1})
+        assert d.keys('foo') == ['foo', 'foobar']
+        assert d.keys('b') == ['bar']
+        assert d.keys('z') == []
 
-    with pytest.raises(OverflowError):
-        dawg.IntDAWG({'f': 2**32-1})
+    def test_completion_dawg_saveload(self):
+        buf = BytesIO()
+        dawg.CompletionDAWG(self.keys).write(buf)
+        buf.seek(0)
 
+        d = dawg.CompletionDAWG()
+        d.read(buf)
 
-#def test_int_keys():
-#    payload = {'foo': 1, 'bar': 5, 'foobar': 3}
-#    d = dawg.IntDAWG(payload)
-#    assert d.keys() == payload.keys()
+        for key in self.keys:
+            assert key in d
 
-def test_completion_dawg_saveload():
-    keys = ['f', 'bar', 'foo', 'foobar']
+        assert d.keys('foo') == ['foo', 'foobar']
+        assert d.keys('b') == ['bar']
+        assert d.keys('z') == []
 
-    buf = BytesIO()
-    dawg.CompletionDAWG(keys).write(buf)
-    buf.seek(0)
+    def test_no_segfaults_on_invalid_file(self):
+        d = self.dawg()
+        fd, path = tempfile.mkstemp()
+        with open(path, 'w') as f:
+            f.write('foo')
 
-    d = dawg.CompletionDAWG()
-    d.read(buf)
+        with pytest.raises(IOError) as e:
+            d.load(path)
+            assert "can't load _dawg.Dictionary" in e.args[0]
 
-    for key in keys:
-        assert key in d
-
-    assert d.keys('foo') == ['foo', 'foobar']
-    assert d.keys('b') == ['bar']
-    assert d.keys('z') == []
--- tests/test_payload_dawg.py
+++ tests/test_payload_dawg.py
@@ -6,66 +6,76 @@ from io import BytesIO
 import pytest
 import dawg
 
-DATA = (
-    ('foo', b'data1'),
-    ('bar', b'data2'),
-    ('foo', b'data3'),
-    ('foobar', b'data4')
-)
+class TestBytesDAWG(object):
 
-STRUCTURED_DATA = (  # payload is (length, vowels count, index) tuple
-    ('foo',     (3, 2, 0)),
-    ('bar',     (3, 1, 0)),
-    ('foo',     (3, 2, 1)),
-    ('foobar',  (6, 3, 0))
-)
+    DATA = (
+        ('foo', b'data1'),
+        ('bar', b'data2'),
+        ('foo', b'data3'),
+        ('foobar', b'data4')
+    )
 
-def test_contains():
-    d = dawg.BytesDAWG(DATA)
-    for key, val in DATA:
-        assert key in d
+    def dawg(self):
+        return dawg.BytesDAWG(self.DATA)
 
-    assert 'food' not in d
-    assert 'x' not in d
-    assert 'fo' not in d
+    def test_contains(self):
+        d = self.dawg()
+        for key, val in self.DATA:
+            assert key in d
+
+        assert 'food' not in d
+        assert 'x' not in d
+        assert 'fo' not in d
 
 
-def test_getitem():
-    d = dawg.BytesDAWG(DATA)
+    def test_getitem(self):
+        d = self.dawg()
 
-    assert d['foo'] == [b'data1', b'data3']
-    assert d['bar'] == [b'data2']
-    assert d['foobar'] == [b'data4']
+        assert d['foo'] == [b'data1', b'data3']
+        assert d['bar'] == [b'data2']
+        assert d['foobar'] == [b'data4']
 
-    with pytest.raises(KeyError):
-        d['f']
+        with pytest.raises(KeyError):
+            d['f']
 
-    with pytest.raises(KeyError):
-        d['food']
+        with pytest.raises(KeyError):
+            d['food']
 
-    with pytest.raises(KeyError):
-        d['foobarz']
+        with pytest.raises(KeyError):
+            d['foobarz']
 
-    with pytest.raises(KeyError):
-        d['x']
+        with pytest.raises(KeyError):
+            d['x']
 
 
-def test_record_getitem():
-    d = dawg.RecordDAWG("=3H", STRUCTURED_DATA)
-    assert d['foo'] == [(3, 2, 0), (3, 2, 1)]
-    assert d['bar'] == [(3, 1, 0)]
-    assert d['foobar'] == [(6, 3, 0)]
+class TestRecordDAWG(object):
 
-def test_record_items():
-    d = dawg.RecordDAWG("=3H", STRUCTURED_DATA)
-    assert sorted(d.items()) == sorted(STRUCTURED_DATA)
+    STRUCTURED_DATA = (  # payload is (length, vowels count, index) tuple
+        ('foo',     (3, 2, 0)),
+        ('bar',     (3, 1, 0)),
+        ('foo',     (3, 2, 1)),
+        ('foobar',  (6, 3, 0))
+    )
 
-def test_record_keys():
-    d = dawg.RecordDAWG("=3H", STRUCTURED_DATA)
-    assert sorted(d.keys()) == ['bar', 'foo', 'foo', 'foobar',]
+    def dawg(self):
+        return dawg.RecordDAWG("=3H", self.STRUCTURED_DATA)
 
-def test_record_keys_prefix():
-    d = dawg.RecordDAWG("=3H", STRUCTURED_DATA)
-    assert sorted(d.keys('fo')) == ['foo', 'foo', 'foobar']
-    assert d.keys('bar') == ['bar']
-    assert d.keys('barz') == []
+    def test_record_getitem(self):
+        d = self.dawg()
+        assert d['foo'] == [(3, 2, 0), (3, 2, 1)]
+        assert d['bar'] == [(3, 1, 0)]
+        assert d['foobar'] == [(6, 3, 0)]
+
+    def test_record_items(self):
+        d = self.dawg()
+        assert sorted(d.items()) == sorted(self.STRUCTURED_DATA)
+
+    def test_record_keys(self):
+        d = self.dawg()
+        assert sorted(d.keys()) == ['bar', 'foo', 'foo', 'foobar',]
+
+    def test_record_keys_prefix(self):
+        d = self.dawg()
+        assert sorted(d.keys('fo')) == ['foo', 'foo', 'foobar']
+        assert d.keys('bar') == ['bar']
+        assert d.keys('barz') == []
