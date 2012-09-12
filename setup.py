4c994be5a77e8fcb4efd418c6b4bfdc0921a397a
Mikhail Korobov <kmike84@gmail.com>
2012-09-13 02:37:49 +0600

    python 3.3 is supported again

--- setup.py
+++ setup.py
@@ -33,6 +33,7 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: Implementation :: CPython',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: Scientific/Engineering :: Information Analysis',
